8c7cc369bd1dad7d22e22600ed8d4633fcdd9b6e
Muhammet Canlı <howardroark756@gmail.com>
2026-01-07 19:25:33 +0100

    refactor: Migrate from Gradle to Maven

--- fintech-core/src/main/java/com/trademaster/fintech_core/TrademasterFintechBotApplication.java
+++ fintech-core/src/main/java/com/trademaster/fintech_core/TrademasterFintechBotApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FintechCoreApplication {
+public class TrademasterFintechBotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FintechCoreApplication.class, args);
+		SpringApplication.run(TrademasterFintechBotApplication.class, args);
 	}
 
 }
