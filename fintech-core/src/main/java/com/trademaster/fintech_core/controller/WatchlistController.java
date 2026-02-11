52218886613183e4d35e08cd1b4c154f63741e5e
Muhammet Canlı <howardroark756@gmail.com>
2026-02-11 23:52:45 +0100

    Update WatchlistController.java

--- fintech-core/src/main/java/com/trademaster/fintech_core/controller/WatchlistController.java
+++ fintech-core/src/main/java/com/trademaster/fintech_core/controller/WatchlistController.java
@@ -1,6 +1,7 @@
 package com.trademaster.fintech_core.controller;
 
 import com.trademaster.fintech_core.domain.entity.Asset;
+import com.trademaster.fintech_core.service.UserAssetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
