f3b8835ca33993d60150b9c9ad7c99cc6a112c6e
Muhammet Canlı <howardroark756@gmail.com>
2026-03-06 23:49:13 +0300

    Create PriceProviderException.java

---
+++ fintech-core/src/main/java/com/trademaster/fintech_core/service/PriceProviderException.java
@@ -0,0 +1,8 @@
+package com.trademaster.fintech_core.service;
+
+public class PriceProviderException extends RuntimeException {
+
+    public PriceProviderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
