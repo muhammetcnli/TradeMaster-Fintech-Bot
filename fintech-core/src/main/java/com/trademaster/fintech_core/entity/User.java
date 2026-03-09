a933f79b6dc09a7c0a370145562a2e2a5640dd8f
Muhammet Canlı <howardroark756@gmail.com>
2026-03-09 23:25:36 +0300

    add one-to-many relationship

--- fintech-core/src/main/java/com/trademaster/fintech_core/entity/User.java
+++ fintech-core/src/main/java/com/trademaster/fintech_core/entity/User.java
@@ -2,13 +2,14 @@ package com.trademaster.fintech_core.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
-@NoArgsConstructor // JPA için zorunludur
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
@@ -17,18 +18,22 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
     private UUID id;
 
     @Column(nullable = false, unique = true)
     private String username;
 
+    // add the standard precision and scale
     @Column(nullable = false, precision = 19, scale = 4)
     private BigDecimal balance;
 
     @Column(nullable = false)
-    private String authProvider;
+    private String authProvider; // "TELEGRAM", "GOOGLE" vb.
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String externalId;
+
+    // add one-to-many relationship
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserAsset> assets = new ArrayList<>();
 }
