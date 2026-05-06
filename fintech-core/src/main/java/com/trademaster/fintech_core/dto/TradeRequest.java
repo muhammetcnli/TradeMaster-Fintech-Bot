b4084d8603507ec0228cef7a386deb025563f99f
Muhammet Canlı <howardroark756@gmail.com>
2026-05-06 17:53:08 +0300

    remove id

--- fintech-core/src/main/java/com/trademaster/fintech_core/dto/TradeRequest.java
+++ fintech-core/src/main/java/com/trademaster/fintech_core/dto/TradeRequest.java
@@ -4,12 +4,10 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 @Data
 @Builder
 public class TradeRequest {
-    private UUID userId;
     private BigDecimal quantity;
 }
 
--- fintech-core/src/main/java/com/trademaster/fintech_core/dto/WatchRequest.java
+++ fintech-core/src/main/java/com/trademaster/fintech_core/dto/WatchRequest.java
@@ -3,12 +3,8 @@ package com.trademaster.fintech_core.dto;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.UUID;
-
 @Data
 @Builder
 public class WatchRequest {
-
-    UUID userId;
     String symbol;
 }
