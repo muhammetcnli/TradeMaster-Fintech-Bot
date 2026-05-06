4ac2ae5bc9b8ead9162c33e01b9591a2933cb60a
Muhammet Canlı <howardroark756@gmail.com>
2026-05-06 17:52:20 +0300

    add sessionToken, isued at, created at

--- fintech-core/src/main/java/com/trademaster/fintech_core/entity/User.java
+++ fintech-core/src/main/java/com/trademaster/fintech_core/entity/User.java
@@ -5,6 +5,7 @@ import lombok.*;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.time.Instant;
 import java.util.UUID;
 
 @Getter
@@ -33,7 +34,24 @@ public class User {
     @Column(nullable = false, unique = true)
     private String externalId;
 
+    @Column(name = "session_token_hash", unique = true)
+    private String sessionTokenHash;
+
+    @Column(name = "session_token_issued_at")
+    private Instant sessionTokenIssuedAt;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
     // add one-to-many relationship
+    @Builder.Default
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<UserAsset> assets = new ArrayList<>();
+
+    @PrePersist
+    protected void onCreate() {
+        if (this.createdAt == null) {
+            this.createdAt = Instant.now();
+        }
+    }
 }
