55c6ff59218b82344e09d7e68f1876f8c779e2d8
Muhammet Canlı <howardroark756@gmail.com>
2026-05-09 12:32:35 +0300

    Create AlertType.java

---
+++ fintech-core/src/main/java/com/trademaster/fintech_core/entity/AlertType.java
@@ -0,0 +1,11 @@
+package com.trademaster.fintech_core.entity;
+
+/**
+ * Types of alerts supported by the system.
+ */
+public enum AlertType {
+    PRICE_ABOVE,
+    PRICE_BELOW,
+    PERCENT_CHANGE_UP,
+    PERCENT_CHANGE_DOWN
+}
